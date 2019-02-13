3eaa55808510550aaff49619e7d1d79c4fccf0ac
Olya Yeremenko <oyeremenko@gmail.com>
2019-02-13 17:13:33 +0400

    5 tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+validates :name, presence: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
+
+  test "#full_name should not crash if user first name is nil" do
+    george = User.new(last_name: 'abitbol')
+    assert_equal "Abitbol", george.full_name
+  end
 end
